10256a1cd597b446027b57cd0651bc30377aa34b
shubhshrma <shubham.31996@gmail.com>
2017-08-30 23:59:05 +0530

    update route and add mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var shortid = require('shortid');
+var validUrl = require('valid-url');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/new/:url(*)', function(req, res, next) {
+	
+  var conn="mongodb://localhost:27017/urlshortener";
+  mongodb.MongoClient.connect(conn,function(err,db){
+  	
+  	if (err) {
+    	console.log("Unable to connect to server", err);
+    } 
+    else {
+    	console.log("Connected to server");
+    	var params = req.params.url;
+    	insertLink={url: params, short: "test"};
+        db.collection('links').insert([insertLink]);
+        res.send(params);
+    };
+    
+
+  });
 });
 
 module.exports = router;
