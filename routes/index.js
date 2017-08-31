4c076b5bd5fcfc7d31462db402e3bee38930bcd0
shubhshrma <shubham.31996@gmail.com>
2017-08-31 23:15:02 +0530

    add route

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ var mongodb = require('mongodb');
 var shortid = require('shortid');
 var validUrl = require('valid-url');
 
+
+var conn="mongodb://localhost:27017/urlshortener";
+
 /* GET home page. */
 router.get('/new/:url(*)', function(req, res, next) {
 	
-  var conn="mongodb://localhost:27017/urlshortener";
+  
   mongodb.MongoClient.connect(conn,function(err,db){
   	
   	if (err) {
@@ -15,14 +18,46 @@ router.get('/new/:url(*)', function(req, res, next) {
     } 
     else {
     	console.log("Connected to server");
+
     	var params = req.params.url;
-    	insertLink={url: params, short: "test"};
-        db.collection('links').insert([insertLink]);
-        res.send(params);
+    	
+      if(validUrl.isUri(params)){
+        var shortCode=shortid.generate();
+
+        var obj={url:params , short:shortCode};
+
+        db.collection('links').insert([obj]);
+        res.json({ originalUrl:params , shortUrl:"localhost:3000/"+shortCode});
+        //closing connection not sure
+        db.close();
+      }
+      else{
+        console.log("Error, make sure you enter a valid URL .");
+      }
     };
     
 
   });
+});
+
+router.get('/:short',function(req,res,next){
+  var conn="mongodb://localhost:27017/urlshortener";
+   mongodb.MongoClient.connect(conn,function(err,db){
+    if (err) {
+      console.log("Unable to connect to server", err);
+    }
+    else{
+      console.log("Connected to server");
+      var params=req.params.short;
+
+      var findLink=function(db,callback){};
+      findLink(db,function(){
+        db.close();
+      });
+
+
+    }
+   });
 });
 
 module.exports = router;
